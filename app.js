9e40c84f6b922e30dc75a706ab984bf37c2a743c
rishabsingh9 <rishabsinghrs1733@gmail.com>
2023-08-28 18:36:26 +0530

    home about using url

--- app.js
+++ app.js
@@ -1,8 +1,14 @@
 const http=require('http');
 const server=http.createServer((req,res)=>{
-  res.end('Rishabh');
+ const url=req.url;
+ if(url=='/home'){
+  res.write('Welcome home')
+ }
+ else if (url === '/about') {
+  res.write('Welcome to About Us page');
+} else if (url === '/node') {
+  res.write('Welcome to my Node.js project');
+} 
+res.end();
 })
-server.on('error', error => {
-    console.error('Server error:', error.message);
-  });
 server.listen(4000);

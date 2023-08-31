75f5da22d105549336f8ffb720794097bea6ec91
rishabsingh9 <rishabsinghrs1733@gmail.com>
2023-08-31 18:44:55 +0530

    middleware added

--- app3.js
+++ app3.js
@@ -1,5 +1,6 @@
 const http = require('http');
 const routes=require('./routes');
+console.log('ok');
 
 const server = http.createServer(routes.handler);
 
---
+++ app4.js
@@ -0,0 +1,13 @@
+const http=require('http')
+
+const express=require('express')
+const app=express();
+app.use((req,res,next)=>{
+    console.log("In the middleware")
+    next();
+})
+app.use((req,res,next)=>{
+    console.log("In the Second middleware")
+})
+const server=http.createServer(app);
+server.listen(4000);

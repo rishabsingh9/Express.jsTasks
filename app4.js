2e1d0d6d6b528ba7447498ff6a778e262fbd7203
rishabsingh9 <rishabsinghrs1733@gmail.com>
2023-09-02 18:53:28 +0530

    Extracting the input submitted

--- app4.js
+++ app4.js
@@ -1,13 +1,18 @@
-const http=require('http')
-
 const express=require('express')
+const parser=require('body-parser');
 const app=express();
-app.use((req,res,next)=>{
-    console.log("In the middleware")
-    next();
+app.use(parser.urlencoded({extended:false}));
+app.use('/addProduct',(req,res,next)=>{
+    res.send('<form action="/product" Method="POST"><input type ="text" name="title"><input type ="text" name="size"><button type="submit">Add Product</button></form>')
 })
-app.use((req,res,next)=>{
-    console.log("In the Second middleware")
+app.use('/product',(req,res,next)=>{
+    console.log(req.body)
+    res.redirect('/')
 })
-const server=http.createServer(app);
-server.listen(4000);
+
+app.use('/',(req,res,next)=>{
+    res.send('<h1>Hello from Expresssssssss!</h1>')
+  //  console.log("In the middleware")
+   // next();
+})
+app.listen(4000);
